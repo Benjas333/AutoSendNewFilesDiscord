c314f433f951da20f5b67f517c07854e712d1108
Benjas333 <seston30@gmail.com>
2024-11-06 00:22:56 -0600

    main.py renamed to selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,10 +1,9 @@
 import config
-from typing import Optional
+# from typing import Optional
 from pathlib import Path
 from time import sleep
 from files_handler import globClips, isFileBeingUsed
-from clips_manipulator import create_clip
-# from discord_webhook import DiscordWebhook
+# from clips_manipulator import create_clip
 from discord.ext import tasks
 import discord
 
@@ -56,68 +55,7 @@ class SelfBot(discord.Client):
         @sendNewClips.before_loop
         async def before_sendNewClips(self):
                 await self.wait_until_ready()
-                        
-        
-        # async def sendMessage(self, message: str, file: Optional[Path] = None):
-        #         channel = self.get_channel(config.CHANNEL_ID)
-        #         print(message)
-        #         if not file:
-        #                 await channel.send(message)
-        #                 return
 
-        #         if not file.is_file():
-        #                 raise Exception("The path provided is not a file: " + file)
-                
-        #         sleep(5)
-        #         res = create_clip(file, 30)
-        #         if res.returncode != 0:
-        #                 raise Exception("Failed to create clip: " + str(res.stderr))
-        #         file = Path(f"clips/{file.name}")
-
-        #         print(f"Sending file: {file.name}")
-        #         await channel.send(message, file=discord.File(file.read_bytes(), file.name))
-
-# webhook = DiscordWebhook(config.WEBHOOK_URL, username="New Clips AutoSender")
 client = SelfBot()
 client.run(config.TOKEN)
 
-
-# def sendMessage(message: str, file: Optional[Path] = None):
-#         print(message)
-#         webhook.content = message
-#         if not file:
-#                 webhook.execute()
-#                 return
-        
-#         if not file.is_file():
-#                 raise Exception("The path provided is not a file: " + file)
-        
-#         sleep(5)
-#         res = create_clip(file, 30)
-#         if res.returncode != 0:
-#                 raise Exception("Failed to create clip: " + str(res.stderr))
-#         file = Path(f"clips/{file.name}")
-
-#         print(f"Sending file: {file.name}")
-#         webhook.add_file(file.read_bytes(), file.name)
-#         webhook.execute()
-#         webhook.remove_files()
-
-
-# def checkForNewFiles():
-#         new_clips = globClips(config.CLIPS_DIRECTORY)
-#         if not new_clips: return
-
-#         client.sendMessage("### New clip(s) detected")
-
-#         for clip in new_clips:
-#                 clipObj = Path(clip)
-#                 client.sendMessage(f"New clip: {clipObj.name}", clipObj)
-
-
-# if __name__ == "__main__":
-#         client.sendMessage("Successfully started webhook")
-
-#         while True:
-#                 sleep(1)
-#                 checkForNewFiles()
