b94bc257e65a5a9b9dbc7efc6fbdf90b9aaa9469
Benjas333 <seston30@gmail.com>
2024-11-07 01:19:39 -0600

    improved logic

--- src/files_handler.py
+++ src/files_handler.py
@@ -28,7 +28,8 @@ files = loadListFromFile()
 def globNewFiles(directory: str, extension: str | list[str], recursive: bool = False):
         directory_path = Path(directory)
         match_strings = [f"*.{extension_iter}" for extension_iter in extension] if isinstance(extension, list) else [f"*.{extension}"]
-        all_files = list(map(lambda file: str(file), chain.from_iterable((directory_path.glob(match_string) for match_string in match_strings) if not recursive else directory_path.rglob(match_string) for match_string in match_strings)))
+        func = directory_path.glob if not recursive else directory_path.rglob
+        all_files = list(map(lambda file: str(file), chain.from_iterable(func(match_string) for match_string in match_strings)))
         new_files = set(all_files).difference(files.copy())
         # updateFile(all_files)
         return new_files
